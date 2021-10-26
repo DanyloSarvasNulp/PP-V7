fbf4fa2b77c2ec5b531c2ea4eb49835dd9acb2b9
Maksym1os <maksym.los.knm.2020.@edu.lpnu.ua>
2021-10-26 13:40:51 +0300

    all is working

--- check_models.py
+++ check_models.py
@@ -1,6 +1,5 @@
 from models import Session, user, access, querry, auditorium
 
-session = Session()
 
 user1 = user(username="pusyBOY",email="customemail@gmail.com", phone="0935215742")
 
@@ -10,14 +9,14 @@ querry1 = querry(place=4)
 
 access1 = access(auditorium_id=1, user_id=1, querry_id=1)
 
-session.add(user1)
-# session.add(auditorium1)
-# session.add(querry1)
-# session.add(access1)
-session.commit()
+
+with Session() as session:
+    session.add(user1)
+    session.add(auditorium1)
+    session.add(querry1)
+    session.add(access1)
+    session.commit()
 
 
-# print(session.query(user).all()[0])
+print(session.query(user).all()[0])
 
-
-session.close()
