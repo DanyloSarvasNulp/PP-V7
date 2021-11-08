e7391907a8b491eca3042d6693ae98c2900b7868
DanyloSarvasNulp <91377012+DanyloSarvasNulp@users.noreply.github.com>
2021-11-08 19:19:44 +0200

    changed requests logic

--- DataBase/Blueprint/accessRequests.py
+++ DataBase/Blueprint/accessRequests.py
@@ -1,39 +1,32 @@
 from app import app
-
-from DataBase.schemas import (
-    AccessSchema)
+from DataBase.schemas import AccessSchema
+from DataBase.models import Access
+from flask import request
 
 from DataBase.db_utils import (
     create_entry,
+    get_entries,
     get_entry_by_ids,
     delete_entry_by_ids,
 )
-
-from DataBase.models import Access
-
-from flask import request, jsonify
 
 
 @app.route("/access", methods=["POST"])  # create new access
 def create_access():
     access_data = AccessSchema().load(request.get_json())
-    access_obj = create_entry(Access, **access_data)
-    return jsonify(AccessSchema().dump(access_obj))
+    return create_entry(Access, AccessSchema, **access_data)
 
 
 @app.route("/access", methods=["GET"])  # get all accesses
 def get_access():
-    access_array = Access.query.all()
-    return jsonify(AccessSchema(many=True).dump(access_array))
+    return get_entries(Access, AccessSchema)
 
 
 @app.route("/access/<int:user_id>,<int:auditorium_id>", methods=["GET"])  # get access by id
 def get_access_by_two_ids(user_id, auditorium_id):
-    access_obj = get_entry_by_ids(Access, user_id, auditorium_id)
-    return jsonify(AccessSchema().dump(access_obj))
+    return get_entry_by_ids(Access, AccessSchema, user_id, auditorium_id)
 
 
 @app.route("/access/<int:user_id>,<int:auditorium_id>", methods=["DELETE"])  # delete access by id
 def delete_access_by_two_ids(user_id, auditorium_id):
-    access_obj = delete_entry_by_ids(Access, user_id, auditorium_id)
-    return jsonify(AccessSchema().dump(access_obj))
+    return delete_entry_by_ids(Access, AccessSchema, user_id, auditorium_id)
--- DataBase/Blueprint/auditoriumRequests.py
+++ DataBase/Blueprint/auditoriumRequests.py
@@ -1,47 +1,39 @@
 from app import app
-
-from DataBase.schemas import (
-    AuditoriumSchema)
+from DataBase.schemas import AuditoriumSchema
+from DataBase.models import Auditorium
+from flask import request
 
 from DataBase.db_utils import (
     create_entry,
+    get_entries,
     get_entry_by_id,
     update_entry_by_id,
     delete_entry_by_id,
 )
 
-from DataBase.models import Auditorium
-
-from flask import request, jsonify
-
 
 @app.route("/auditorium", methods=["POST"])  # create new auditorium
 def create_auditorium():
     auditorium_data = AuditoriumSchema().load(request.get_json())
-    auditorium_obj = create_entry(Auditorium, **auditorium_data)
-    return jsonify(AuditoriumSchema().dump(auditorium_obj))
+    return create_entry(Auditorium, AuditoriumSchema, **auditorium_data)
 
 
 @app.route("/auditorium", methods=["GET"])  # get all auditoriums
 def get_auditorium():
-    auditorium_array = Auditorium.query.all()
-    return jsonify(AuditoriumSchema(many=True).dump(auditorium_array))
+    return get_entries(Auditorium, AuditoriumSchema)
 
 
 @app.route("/auditorium/<int:id>", methods=["GET"])  # get auditorium by id
 def get_auditorium_by_id(id):
-    auditorium_obj = get_entry_by_id(Auditorium, id)
-    return jsonify(AuditoriumSchema().dump(auditorium_obj))
+    return get_entry_by_id(Auditorium, AuditoriumSchema, id)
 
 
 @app.route("/auditorium/<int:id>", methods=["PUT"])  # update auditorium by id
 def update_auditorium_by_id(id):
     auditorium_data = AuditoriumSchema().load(request.get_json())
-    auditorium_obj = update_entry_by_id(Auditorium, id, **auditorium_data)
-    return jsonify(AuditoriumSchema().dump(auditorium_obj))
+    return update_entry_by_id(Auditorium, AuditoriumSchema, id, **auditorium_data)
 
 
 @app.route("/auditorium/<int:id>", methods=["DELETE"])  # delete auditorium by id
 def delete_auditorium_by_id(id):
-    auditorium_obj = delete_entry_by_id(Auditorium, id)
-    return jsonify(AuditoriumSchema().dump(auditorium_obj))
+    return delete_entry_by_id(Auditorium, AuditoriumSchema, id)
